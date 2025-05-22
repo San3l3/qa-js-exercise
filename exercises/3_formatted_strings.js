9f3a1805b85a7e36163d02d1a8f5ea322e367f25
San3l3 <oiiwagwan07@gmail.com>
2025-05-22 08:26:10 +0200

    Complete exercise3

--- exercises/3_formatted_strings.js
+++ exercises/3_formatted_strings.js
@@ -3,6 +3,14 @@ const exercise3 = ()=>
         console.log('Exercise 3');
 
         // Please Complete Exercise 3 here
+        let test = "registration";
+        let testerName = "Sanele";
+        let duration = 5;
+        let results = "passed"
+
+        //constructed sentence
+        const report = `The ${test} test performed by ${testerName} finally ${results} after ${duration} hours`;
+        console.log(report);
 
         // End of Exercise 3
         console.log("Exercise 3 completed");
