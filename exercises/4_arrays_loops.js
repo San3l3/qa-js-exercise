9884a818e866f290bd28019306cee129cdf98623
San3l3 <oiiwagwan07@gmail.com>
2025-05-22 08:27:00 +0200

    Complete exercise4

--- exercises/4_arrays_loops.js
+++ exercises/4_arrays_loops.js
@@ -3,7 +3,14 @@ const exercise4 = ()=>
         console.log('Exercise 4');
 
         // Please Complete Exercise 4 here
+        const testSteps = [ "Click on 'My Account' Drop menu", "Click on 'Register'", "Do not enter any input", "Click on 'Continue' button"]
+        testSteps.forEach(step => console.log(step));
+        const uppercaseTestSteps = testSteps.map(steps => steps.toUpperCase());
+        console.log(uppercaseTestSteps);
 
+        for (let i = 0; i < testSteps.length; i++) {
+            console.log(`In this step we ${testSteps[i]}. `);
+        }
         // End of Exercise 4
         console.log("Exercise 4 completed");
     }   
