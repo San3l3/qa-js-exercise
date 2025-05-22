f52395e76c47d2c4790f0da2b67d1526ae0dbc86
San3l3 <oiiwagwan07@gmail.com>
2025-05-22 08:25:27 +0200

    Complete exercise2

--- exercises/2_strings.js
+++ exercises/2_strings.js
@@ -4,6 +4,20 @@ const exercise2 = ()=>
 
         // Please Complete Exercise 2 here
 
+        //String representing a response
+        let userRes = "I have used API's before.";
+
+        //Clean up whitespaces and convert it to lowercase
+        userRes.trim(" ").toLowerCase();
+
+        //Check if cetain keywords exist
+        userRes.includes("before");
+
+        // replace part of a string 
+        userRes.replace("used", "utalised")
+
+        
+
         // End of Exercise 2
         console.log("Exercise 2 completed");
     }
