bedb2a2ff9e0674ef9dc64557ed1c0c8b1533e7e
San3l3 <oiiwagwan07@gmail.com>
2025-05-22 13:09:51 +0200

    Complete exercise5

--- exercises/5_functions.js
+++ exercises/5_functions.js
@@ -3,6 +3,44 @@ const exercise5 = ()=>
         console.log('Exercise 5');
 
         // Please Complete Exercise 5 here
+        // logResult("Performance", 8);
+        // logResult("Performance", 3);
+
+        //function that logs the testName and status
+        function logResult(testName, status) {
+            //Checking testing time and log the report
+            if (Loadtime > 5) {
+                console.log(`${testName} : ${status}`);
+            }
+            else {
+                console.log(`${testName} : ${status}`)
+            }
+
+        }
+
+        //test report object
+        const testCase = {
+            testName: "compatibility",
+            testId: 1,
+            testerName: "Sanele",
+            status: "passed"
+        }
+
+        //function to return a formated report froma test case object
+        console.log(testReport(testCase));
+        function testReport(rep) {
+            return `Test Name:  ${testCase.testName}\nTestID: ${testCase.testId}\nTester: ${testCase.testName}\nStatus: ${testCase.status}`
+        }
+
+        //function to log elapse time
+
+        console.log(calculateDuration(7, 16));
+        function calculateDuration(startTime, endTime) {
+            let elapsedTime = endTime - startTime;
+            return `The testing process took ${elapsedTime} hours`;
+        }
+
+        
 
         // End of Exercise 5
         console.log("Exercise 5 completed");
